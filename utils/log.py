cda2f40d2a4da1a68f844fafbbcd3a99cbccae56
txlyre <me@txlyre.website>
2024-06-07 10:15:10 +0300

    Clnp.

--- utils/log.py
+++ utils/log.py
@@ -3,4 +3,7 @@ from bot import bot
 async def log(text, channel=bot.log_channel, **kwargs):
     text = text.format(**kwargs)
 
+    if isinstance(channel, int):
+        channel = await bot.fetch_channel(channel)
+
     await channel.send(text)
