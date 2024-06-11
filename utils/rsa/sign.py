3fc017669f2376203897038af3adc9728decf665
txlyre <me@txlyre.website>
2024-06-11 22:36:10 +0300

    Fix.

--- utils/rsa/sign.py
+++ utils/rsa/sign.py
@@ -9,8 +9,7 @@ from utils.encoding.non_printable import NonPrintableEncoder
 
 CHECKSUM_LENGTH = 20
 TS_LENGTH = 8
-TAG_LENGTH = 16
-SIGN_LENGTH = CHECKSUM_LENGTH + TS_LENGTH + TAG_LENGTH
+SIGN_LENGTH = CHECKSUM_LENGTH + TS_LENGTH
 TS_DIFF = 3
 KEY = config.sign_key
 
@@ -19,8 +18,8 @@ class RSASign:
     def sign(message):
         sign = SHA1.new(message.encode('UTF-8')).digest() + struct.pack('<Q', int(time.time()))
         cipher = AES.new(KEY, AES.MODE_EAX, nonce=KEY)
-        sign, tag = cipher.encrypt_and_digest(sign)
-        sign = NonPrintableEncoder.encode_raw(sign + tag)
+        sign = cipher.encrypt(sign)
+        sign = NonPrintableEncoder.encode_raw(sign)
 
         return message + sign
 
@@ -31,14 +30,8 @@ class RSASign:
         
         message, sign = message[:-SIGN_LENGTH], message[-SIGN_LENGTH:]
         sign = NonPrintableEncoder.decode_raw(sign)
-        sign, tag = sign[:-TAG_LENGTH], sign[-TAG_LENGTH:]
         cipher = AES.new(KEY, AES.MODE_EAX, nonce=KEY)
         sign = cipher.decrypt(sign)
-
-        try:
-            cipher.verify(tag)
-        except ValueError:
-            return False
         
         checksum, ts = sign[:-TS_LENGTH], sign[-TS_LENGTH:]
 
