92a79075a40aa0cf8f5276e3efbb4ee6627c183c
txlyre <me@txlyre.website>
2024-06-13 22:06:36 +0300

    Fix.

--- utils/tupper_template.py
+++ utils/tupper_template.py
@@ -1,5 +1,6 @@
 from localization import locale
 
+
 async def get_template_start(user, text):
     text = text.lstrip()
 
@@ -8,8 +9,16 @@ async def get_template_start(user, text):
 
         if text.startswith(l):
             return tupper, l, r
-        
+
     return None
+
+
+def split_template(text):
+    text = text.strip()
+
+    l, r = text.split("text", 1)
+
+    return l.lstrip(), r.strip()
 
 
 def validate_template(text):
@@ -18,20 +27,10 @@ def validate_template(text):
     if "text" not in text:
         text += "text"
 
-    if len(text) <= 4:
+    parts = split_template(text)
+    if len(parts) != 2 or not parts[0]:
         raise SyntaxError(
             locale.template_should_contain_something_aside_from_placeholder
         )
 
-    if not split_template(text)[0]:
-        raise SyntaxError(
-            locale.template_should_contain_something_aside_from_placeholder
-        )
-    
     return text
-
-
-def split_template(text):
-    text = text.strip()
-
-    return text.split("text", 1)
