9c589b72f833b916d64eebf4c49caa4be00518e1
txlyre <me@txlyre.website>
2024-07-10 15:43:53 +0300

    fix

--- utils/rand.py
+++ utils/rand.py
@@ -1,4 +1,5 @@
 import random
+import struct
 
 import aiohttp
 from loguru import logger
@@ -11,10 +12,6 @@ class RandomSource:
     RANDOMORG = "random.org"
     YEBISU = "yebi.su"
 
-
-# TODO make seed base generator and timeout to update
-# TODO make count for generate numbers
-# TODO make optimal class?
 
 async def _pyrandom(min, max, is_dice=False):
     if is_dice:
@@ -30,32 +27,54 @@ async def _pyrandom(min, max, is_dice=False):
 async def _get(url):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
-            return await resp.text()
+            return await resp.read()
+
+
+async def generate(source, min, max, count=1):
+    total = count * 8
+    data = await _get(f"{source}{total}")
+
+    numbers = []
+    for i in range(count):
+        number = struct.unpack("<Q", data[i * 8 : i * 8 + 8])[0]
+
+        numbers.append(number % (max + 1 - min) + min)
+
+    return numbers
 
 
 async def _randomorg(min, max, is_dice=False):
-    random_hex_string = await _get(
-        f"https://www.random.org/cgi-bin/randbyte?nbytes=32&format=h"
-    )
-    random_hex_string = random_hex_string.replace(" ", "")
-    random.seed(random_hex_string)
     if is_dice:
-        number_list = [random.randint(1, max) for _ in range(min)]
-        return number_list
+        count = min
+        min = 1
+        max = max
+    else:
+        count = 1
 
-    return random.randint(min, max)
+    numbers = await generate(
+        "https://www.random.org/cgi-bin/randbyte?nbytes=", min, max, count
+    )
+
+    if count == 1:
+        return numbers[0]
+
+    return numbers
 
 
 async def _trng_yebisu(min, max, is_dice=False):
-    random_hex_string = await _get(
-        f"https://yebi.su/api/pool?count=32&format=hexstring&mode=trng"
-    )
-    random.seed(random_hex_string)
     if is_dice:
-        number_list = [random.randint(1, max) for _ in range(min)]
-        return number_list
+        count = min
+        min = 1
+        max = max
+    else:
+        count = 1
 
-    return random.randint(min, max)
+    numbers = await generate(f"https://yebi.su/api/pool?count=", min, max, count)
+
+    if count == 1:
+        return numbers[0]
+
+    return numbers
 
 
 SOURCES = {
